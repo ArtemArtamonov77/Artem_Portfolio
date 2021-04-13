583f5877af12933b86e74c7ddd29faefa5ee9800
Артамонов Артём Александрович <ArtemWebMastering@gmail.com>
2021-04-13 19:38:43 +0300

    сделали два блока

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+/* Скрипт добавляет/удаляет класс активности меню */
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
